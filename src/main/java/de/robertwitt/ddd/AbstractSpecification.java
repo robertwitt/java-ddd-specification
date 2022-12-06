bf4e27b505f9686007570537de4cbf44d290d6c9
robertwitt <robert_witt@icloud.com>
2022-12-06 13:56:21 +0100

    Abstract throwIfUnsatisfiedBy

--- src/main/java/de/robertwitt/ddd/AbstractSpecification.java
+++ src/main/java/de/robertwitt/ddd/AbstractSpecification.java
@@ -6,6 +6,13 @@ package de.robertwitt.ddd;
 public abstract class AbstractSpecification<T> implements Specification<T> {
 
   @Override
+  public void throwIfUnsatisfiedBy(T candidate) throws IllegalArgumentException {
+    if (!isSatisfiedBy(candidate)) {
+      throw new IllegalArgumentException(String.format("Specification not satisfied by value '%s'", candidate));
+    }
+  }
+
+  @Override
   public Specification<T> and(Specification<T> other) {
     return new AndSpecification<>(this, other);
   }
--- src/test/java/de/robertwitt/ddd/impl/EvenIntegerSpecification.java
+++ src/test/java/de/robertwitt/ddd/impl/EvenIntegerSpecification.java
@@ -3,17 +3,8 @@ package de.robertwitt.ddd.impl;
 import de.robertwitt.ddd.AbstractSpecification;
 
 public class EvenIntegerSpecification extends AbstractSpecification<Integer> {
-
   @Override
   public boolean isSatisfiedBy(Integer candidate) {
     return candidate % 2 == 0;
   }
-
-  @Override
-  public void throwIfUnsatisfiedBy(Integer candidate) throws IllegalArgumentException {
-    if (!isSatisfiedBy(candidate)) {
-      throw new IllegalArgumentException();
-    }
-  }
-
 }
